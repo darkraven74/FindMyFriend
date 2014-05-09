c441a2cfe78c3f4918c207088589214e61d4c2a4
Vladimir Baev <darkraven8@gmail.com>
2014-05-09 14:17:26 +0400

    OkFriends added

--- src/main/java/ru/ifmo/findmyfriend/friendlist/FriendData.java
+++ src/main/java/ru/ifmo/findmyfriend/friendlist/FriendData.java
@@ -8,12 +8,19 @@ public class FriendData {
     private String name;
     private double latitude;
     private double longitude;
+    private String imageUrl;
 
     public FriendData(long id, String name, double latitude, double longitude) {
         this.id = id;
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    public FriendData(long id, String name, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
     }
 
     public long getId() {
@@ -30,5 +37,9 @@ public class FriendData {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
