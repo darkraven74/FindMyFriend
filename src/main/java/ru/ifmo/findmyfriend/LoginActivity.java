9b77aa9cbcdf6e1caef61c2d0b48b464e44541fa
Vladimir Baev <darkraven8@gmail.com>
2014-05-19 13:42:15 +0400

    actionbar && MyLocation fixed

--- src/main/java/ru/ifmo/findmyfriend/LoginActivity.java
+++ src/main/java/ru/ifmo/findmyfriend/LoginActivity.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import org.json.JSONException;
 import org.json.JSONObject;
